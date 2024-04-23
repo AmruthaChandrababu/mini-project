866ee14a669f6d21d7f48a4d4bfb74fc3e4a470e
AmruthaChandrababu <96697878+AmruthaChandrababu@users.noreply.github.com>
2024-04-23 22:22:15 +0530

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -1,23 +1,21 @@
 import requests
 from bs4 import BeautifulSoup
 
-def scrape_nitnagpur():
-    url = 'https://vnit.ac.in/category/events/'
+def scrape_nitsurathkal():
+    url = 'https://www.nitk.ac.in/upcoming_events'
     response = requests.get(url)
     if response.status_code == 200:
         text = response.content
         data = BeautifulSoup(text, 'html.parser')
-        events = data.find_all(class_="entry-article-part entry-article-header")
+        events = data.find_all(class_="gdlr-core-event-item-content-wrap")
         event_list = []
         for event in events:
-            results = event.find(class_="entry-title entry--item")
-            event_text = results.get_text()
-            cleaned_event = ' '.join(event_text.split())
-            event_list.append(cleaned_event)
+            event_text = event.get_text().strip()
+            event_list.append(event_text)
         return event_list
     else:
         return []
 
-events = scrape_nitnagpur()
+events = scrape_nitsurathkal()
 for event in events:
     print(event)
